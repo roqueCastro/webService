1e744eda3d48dc68419631f3dcd602cbdd14a804
roque <rokke1558@gmail.com>
2018-08-13 21:21:37 -0500

    register evento

--- wsJSONRegistroEvento.php
+++ wsJSONRegistroEvento.php
@@ -2,30 +2,28 @@
 
 require 'conexion.php';
 $encuesta = $_POST['encuesta'];
-$cx = $_POST['cx'];
-$cy = $_POST['cy'];
-$imagen = $_POST['imagen'];
+$cx       = $_POST['cx'];
+$cy       = $_POST['cy'];
+$imagen   = $_POST['imagen'];
 /*$encuesta = 2;
 $cx = "12.6846846";
 $cy = "-87.168435";*/
-$path = "imagenes/Foto_$cx.jpg";
+$path   = "imagenes/Foto_$cx.jpg";
+$actual = date("Y-m-d H:i:s");
 
 //$url = "http://$hostname_localhost/ejemploBDRemota/$path";
-$url = "imagenes/Foto_$cx.jpg";
+$url = "imagenes/Foto_$actual.jpg";
 
-file_put_contents($path,base64_decode($imagen));
-$bytesArchivo=file_get_contents($path);
+file_put_contents($path, base64_decode($imagen));
+$bytesArchivo = file_get_contents($path);
 
-
-$sql="INSERT INTO evento (latitud, longitud, encuesta, usuario, imagen)VALUES (:lat,:lng,:encu,:usu,:image)";
-	$resul= $base->prepare($sql);
-	$resul->execute(array(":lat"=>$cx, ":lng"=>$cy, ":encu"=>$encuesta, ":usu"=>1, ":image"=>$url));
-	$c=$resul->rowCount();
-if($c>0){
-	$idReciente = $base->lastInsertId("evento");
-	echo $idReciente;
-}else{
-	echo 'Noregistra'; 
+$sql   = "INSERT INTO evento (latitud, longitud, encuesta, usuario, imagen)VALUES (:lat,:lng,:encu,:usu,:image)";
+$resul = $base->prepare($sql);
+$resul->execute(array(":lat" => $cx, ":lng" => $cy, ":encu" => $encuesta, ":usu" => 1, ":image" => $url));
+$c = $resul->rowCount();
+if ($c > 0) {
+    $idReciente = $base->lastInsertId("evento");
+    echo $idReciente;
+} else {
+    echo 'Noregistra';
 }
-
-?>
